a6856dddd759d8643bafc3c0690aa04197cfb76d
Fil <88441203+FilipsCalijs@users.noreply.github.com>
2024-01-07 17:45:53 +0200

    add html/scss template for marvel site and create a db servise with api key

---
+++ src/services/MarvelService.js
@@ -0,0 +1,24 @@
+
+
+class MarvelService {
+      
+    getResource = async (url) => {
+        let res = await fetch(url);
+    
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+    
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
+}
+
+export default MarvelService; 
