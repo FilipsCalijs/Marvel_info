15f16e84f50db96759902b7cb668d9fc9c9bc696
Fil <88441203+FilipsCalijs@users.noreply.github.com>
2024-01-09 19:54:19 +0200

    done with randomchar

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -12,34 +12,27 @@ class RandomChar extends Component {
     }
 
     state = {
-        name: null,
-        description: null,
-        thumbnail: null,
-        homepage: null,
-        wiki: null, 
+       char: {}
     }
     
     marvelService = new MarvelService();
 
+    onCharLoaded = (char) =>{
+        this.setState({char})
+    }
+
     updateChar = () =>{
-        const id = 1011005;
+        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
         this.marvelService
             .getCharacter(id)
-            .then(res => {
-                this.setState({
-                    name: res.data.results[0].name,
-                    description: res.data.results[0].description,
-                    thumbnail: res.data.results[0].thumbnail.path + '.' + res.data.results[0].thumbnail.extension,
-                    homepage: res.data.results[0].urls[0].url,
-                    wiki: res.data.results[0].urls[1].url, 
-                })
-            })
+            .then(this.onCharLoaded)
+
     }
 
 
 
     render(){
-        const {name, description, thumbnail, homepage, wiki} = this.state;
+        const {char:{name, description, thumbnail, homepage, wiki}} = this.state;
         return (
             <div className="randomchar">
                 <div className="randomchar__block">
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -15,12 +15,25 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this.__transofrmCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this.__transofrmCharacter(res.data.results[0]);
+    }
+
+    __transofrmCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url, 
+        }
+
     }
 }
 
