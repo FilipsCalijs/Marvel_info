ae07f4f6458f5f34027c83163c1c5276875cbaf9
Fil <88441203+FilipsCalijs@users.noreply.github.com>
2024-02-26 12:43:15 +0200

    done with homework

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,42 +1,44 @@
+import { useState } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
-import ErrorBoudary from "../errorBoundary/ErrorBoudary" 
-import { useState } from "react";
+import ComicsList from "../comicsList/ComicsList";
+import AppBanner from "../appBanner/AppBanner";
+import ErrorBoundary from "../errorBoundary/ErrorBoudary";
 
 
 import decoration from '../../resources/img/vision.png';
 
-
-
-const App = () =>{
+const App = () => {
+    
     const [selectedChar, setChar] = useState(null);
 
-    
-    const onCharSelected = (id) =>{
+    const onCharSelected = (id) => {
         setChar(id);
     }
+
     return (
         <div className="app">
             <AppHeader/>
             <main>
-                <ErrorBoudary>
+                {/* <ErrorBoundary>
                     <RandomChar/>
-                </ErrorBoudary>
+                </ErrorBoundary>
                 <div className="char__content">
-                <ErrorBoudary>
-                    <CharList onCharSelected={onCharSelected}/>
-                </ErrorBoudary>
-                    <ErrorBoudary><CharInfo charId={selectedChar}/></ErrorBoudary>
-                    
+                    <ErrorBoundary>
+                        <CharList onCharSelected={onCharSelected}/>
+                    </ErrorBoundary>
+                    <ErrorBoundary>
+                        <CharInfo charId={selectedChar}/>
+                    </ErrorBoundary>
                 </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
+                <img className="bg-decoration" src={decoration} alt="vision"/> */}
+                <AppBanner/>
+                <ComicsList/>
             </main>
         </div>
-        )
-    
-    
+    )
 }
 
 export default App;
--- src/components/errorBoundary/ErrorBoudary.js
+++ src/components/errorBoundary/ErrorBoudary.js
@@ -1,26 +1,25 @@
-import ErrorMessage from "../errorMessage/ErrorMessage";
 import { Component } from "react";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 
-class ErrorBoudary extends Component{
+class ErrorBoundary extends Component {
     state = {
         error: false
     }
 
-    componentDidCatch(error,errorInfo) {
+    componentDidCatch(error, errorInfo) {
         console.log(error, errorInfo);
         this.setState({
-            error:true
+            error: true
         })
     }
 
     render() {
-        if (this.state.error){
-            return <ErrorMessage></ErrorMessage>
+        if (this.state.error) {
+            return <ErrorMessage/>
         }
+
         return this.props.children;
- 
     }
 }
 
-
-export default ErrorBoudary;
+export default ErrorBoundary;
