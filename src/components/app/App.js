a5558f291bf9e29aa489db38347e85057878d039
Fil <88441203+FilipsCalijs@users.noreply.github.com>
2024-02-27 22:05:34 +0200

    Add route and switch (Route Mode 5.0.3)

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import AppHeader from "../appHeader/AppHeader";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
@@ -9,6 +12,7 @@ import ErrorBoundary from "../errorBoundary/ErrorBoudary";
 
 
 import decoration from '../../resources/img/vision.png';
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
     
@@ -19,25 +23,35 @@ const App = () => {
     }
 
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                {/* <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/> */}
-                <AppBanner/>
-                <ComicsList/>
-            </main>
-        </div>
+    
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Switch>
+                    <Route exact path="/">   
+                        <ErrorBoundary>
+                            <RandomChar/>
+                        </ErrorBoundary>
+                        <div className="char__content">
+                            <ErrorBoundary>
+                                <CharList onCharSelected={onCharSelected}/>
+                            </ErrorBoundary>
+                            <ErrorBoundary>
+                                <CharInfo charId={selectedChar}/>
+                            </ErrorBoundary>
+                        </div>
+                        <img className="bg-decoration" src={decoration} alt="vision"/>
+                    </Route>
+                    <Route exact path="/comics">
+                        <AppBanner/>
+                        <ComicsList/>
+                    </Route>
+                    </Switch>
+                </main>
+                
+            </div>
+        </Router>
     )
 }
 
--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,18 +1,19 @@
 import './appHeader.scss';
+import { Link } from 'react-router-dom';
 
 const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <a href="#">
+                <Link to="/">
                     <span>Marvel</span> information portal
-                </a>
+                </Link>
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><a href="#">Characters</a></li>
+                    <li><Link href="/">Characters</Link></li>
                     /
-                    <li><a href="#">Comics</a></li>
+                    <li><Link to="/comics">Comics</Link></li>
                 </ul>
             </nav>
         </header>
