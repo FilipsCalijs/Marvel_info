b049e1cd8041e50dacb070dc806ef3aa03b84bba
Fil <88441203+FilipsCalijs@users.noreply.github.com>
2024-02-28 22:42:17 +0200

    add new version of router 6

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,55 +1,20 @@
-import { useState } from "react";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import {MainPage, ComicsPage} from '../pages';
 import AppHeader from "../appHeader/AppHeader";
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-
-
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ComicsList from "../comicsList/ComicsList";
-import AppBanner from "../appBanner/AppBanner";
-import ErrorBoundary from "../errorBoundary/ErrorBoudary";
-
-
-import decoration from '../../resources/img/vision.png';
-import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
     
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
-
     return (
-    
         <Router>
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Switch>
-                    <Route exact path="/">   
-                        <ErrorBoundary>
-                            <RandomChar/>
-                        </ErrorBoundary>
-                        <div className="char__content">
-                            <ErrorBoundary>
-                                <CharList onCharSelected={onCharSelected}/>
-                            </ErrorBoundary>
-                            <ErrorBoundary>
-                                <CharInfo charId={selectedChar}/>
-                            </ErrorBoundary>
-                        </div>
-                        <img className="bg-decoration" src={decoration} alt="vision"/>
-                    </Route>
-                    <Route exact path="/comics">
-                        <AppBanner/>
-                        <ComicsList/>
-                    </Route>
-                    </Switch>
+                    <Routes>
+                        <Route exact path="/" element={<MainPage/>}></Route>
+                        <Route exact path="/comics" element={<ComicsPage/>}></Route>
+                    </Routes>
                 </main>
-                
             </div>
         </Router>
     )
--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,5 +1,5 @@
+import {Link, NavLink} from 'react-router-dom';
 import './appHeader.scss';
-import { Link } from 'react-router-dom';
 
 const AppHeader = () => {
     return (
@@ -11,9 +11,15 @@ const AppHeader = () => {
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><Link href="/">Characters</Link></li>
+                    <li><NavLink 
+                    end
+                    style={({ isActive} )=> ({color: isActive ? '#9f0013' : 'inherit'})}
+                    
+                    to="/">Characters</NavLink></li>
                     /
-                    <li><Link to="/comics">Comics</Link></li>
+                    <li><NavLink end
+                    style={({ isActive} )=> ({color: isActive ? '#9f0013' : 'inherit'})}
+                     to="/comics">Comics</NavLink></li>
                 </ul>
             </nav>
         </header>
