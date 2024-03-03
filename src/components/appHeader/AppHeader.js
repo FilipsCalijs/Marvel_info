39e5a32ea24f6656e28ccc12a150b07972a9338a
FilipsCalijs <filipscalijs@gmail.com>
2024-03-03 16:47:26 +0200

    single page is done with hooks

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -17,7 +17,7 @@ const AppHeader = () => {
                     
                     to="/">Characters</NavLink></li>
                     /
-                    <li><NavLink end
+                    <li><NavLink 
                     style={({ isActive} )=> ({color: isActive ? '#9f0013' : 'inherit'})}
                      to="/comics">Comics</NavLink></li>
                 </ul>
